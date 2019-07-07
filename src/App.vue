3c0576901060417520f46f0038eea05196bb1afa
Your Name <11@111.com>
2019-07-07 21:24:39 +0800

    0707

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="app">
     <router-view />
-    <FooterGuide v-if="$route.meta.isShowFooter"/>
+    <FooterGuide v-if="$route.meta.isShowFooter" />
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import FooterGuide from './components/FooterGuide/FooterGuide'
-  export default {
-    name: "App",
-    data () {
-      return {
-        msg: 'App Component'
-      }
-    },
-    components: {
-      FooterGuide
-    }
+import FooterGuide from "./components/FooterGuide/FooterGuide";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "App Component"
+    };
+  },
+
+  async mounted() {
+    this.$store.dispatch("getAddress");
+  },
+
+  components: {
+    FooterGuide
   }
+};
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus" scoped>
-</style>
+<style lang="stylus" rel="stylesheet/stylus" scoped></style>
--- src/main.js
+++ src/main.js
@@ -3,19 +3,35 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Header from './components/Header/Header.vue'
+import ajax from './api/ajax'
+import store from './vuex/store'
+import Star from './components/Star/Star.vue'
 
+//Vue.prototype.$ajax = ajax
 
 // 注册全局组件
 Vue.component('Header', Header)
+Vue.component('Star', Star)
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  /* components: {
+
+
+//第一种写法
+/* new Vue({
+  el:'#app'
+  components:{
     App
   },
-  template: '<App/>' */
-  // render: createElement => createElement(App)  // <App/>
-  render: h => h(App),  // <App/>
+  template:'<App/>'
+  router,
+}) */
+
+
+//第二种写法
+new Vue({
+  el: '#app',
+  // render: createElement => createElement(App)  Es6语法// <App/>
+  render: h => h(App), // <App/>
   router, // 配置路由器
+  store,
 })
